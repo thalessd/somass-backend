0de5754c1c0f64eb1bd75d043f23567edf505d95
Thales Carvalho <thalessd@live.com>
2020-09-21 02:08:30 -0300

    Adição de bibliotecas de segurança ao app

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './shared/filters/all-exceptions.filter';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  });
+
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors(); // Somente para desenvolvimento
-  await app.listen(3000);
+
+  await app.listen(4003);
 }
 
 bootstrap();
